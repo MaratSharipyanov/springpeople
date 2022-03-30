e61f455282e6af4a5eb68363ad0dd9cf93ed3650
maratti8@yandex.ru <maratti8@yandex.ru>
2022-03-30 19:30:10 +0400

    1 commit

---
+++ src/main/java/com/example/springpeople/SpringpeopleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springpeople;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringpeopleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringpeopleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springpeople/SpringpeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springpeople;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringpeopleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
