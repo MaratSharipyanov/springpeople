b49d6ae6fc1e800e326f61c2e7dc61c582be51c5
MaratSharipyanov <maratti8@yandex.ru>
2022-04-13 20:13:06 +0300

    9 commit

--- src/main/java/com/example/springpeople/controller/RegistrationController.java
+++ src/main/java/com/example/springpeople/controller/RegistrationController.java
@@ -25,7 +25,7 @@ public class RegistrationController {
     public String addUser(User user, Map<String, Object> model) {
         User userFromDB = userRepository.findByUsername(user.getUsername());
         if (userFromDB != null) {
-            model.put("message", "User exists!");
+            model.put("message", "Пользователь существует");
             return "registration";
         }
         user.setActive(true);
